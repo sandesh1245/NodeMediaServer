0029e0b23f0b1f7030829d309c70e116ba0b45c4
Mingliang Chen <illuspas@gmail.com>
2019-02-21 11:22:53 +0800

    f

--- test/relay.js
+++ test/relay.js
@@ -2,9 +2,9 @@ const NodeHttpRelay = require('../src/node_http_relay');
 
 let nhr = new NodeHttpRelay();
 
-nhr.play('http://192.168.0.8:8000/live/s.flv');
-// nhr.publish('http://192.168.0.8:8000/live/s.flv');
-nhr.publish('http://192.168.0.8:8000/live/s1.flv');
+nhr.pull('http://192.168.0.8:8000/live/s.flv');
+// nhr.push('http://192.168.0.8:8000/live/s.flv');
+nhr.push('http://192.168.0.8:8000/live/s1.flv');
 nhr.start();
 nhr.on('start',()=>{
   console.log('relay start');
